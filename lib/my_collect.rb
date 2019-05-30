8655e60f2c96eb12a73e1589dad7bb9309373777
Kelly Dobbins <kellydobbins26@outlook.com>
2019-05-30 14:53:36 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+def my_collect(empy_array)
+ empty_array = []
+counter = 0
+my_collect(empty_array) do |x|
+counter += 1
+end 
+empty_array
+end 
 
-
+def  my_collect(languages) 
+languages = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(languages) do |language|
+languages.map{ |language| language.capitalize}
+end 
+end 
